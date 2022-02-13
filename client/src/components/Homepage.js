e8164a4ea076e4f5b219012dfea7335d9cbac584
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2022-02-12 21:57:22 -0500

    Hompage.js description/rules update

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -5,7 +5,25 @@ function Homepage() {
     return (
       <div className="homepage">
 
+        <h1>Welcome!</h1>
+        <p>You have been assigned a mock credit score of 680.</p>
         <p>
+          As you explore the educational material found by clicking the Learn link,
+          you will be prompted with two scenarios per section. Select one select one 
+          scenario before moving on to the next section.
+          Your mock credit score will change as you make choices. Hopefully this experience
+          teaches not only informtion about credit, but the power of decision making. 
+          Happy Learning!
+        </p>
+        
+      </div>
+    );
+  }
+
+  export default Homepage;
+
+
+   {/* <p>
                 A one of a kind credit education experience... I think.
         </p>
             created by 
@@ -20,10 +38,4 @@ function Homepage() {
               </a>
         <p>
             huge ego alert^
-        </p>
-        
-      </div>
-    );
-  }
-
-  export default Homepage;
+        </p> */}
