2258940f2b3ca3edb38025b71e3d54a387afab5a
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2022-02-12 23:28:04 -0500

    Hompage.js description/rules update

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -5,13 +5,15 @@ function Homepage() {
     return (
       <div className="homepage">
 
-        <h1>Welcome!</h1>
-        <p>You have been assigned a mock credit score of 680.</p>
-        <p>
+        <h1 className="welcome">Welcome!</h1>
+        <p className="rules">You have been assigned a mock credit score of 680.
+        <br></br><br></br>
           As you explore the educational material found by clicking the Learn link,
           you will be prompted with two scenarios per section. Select one select one 
           scenario before moving on to the next section.
-          Your mock credit score will change as you make choices. Hopefully this experience
+          Your mock credit score will change as you make choices.
+          <br></br><br></br>
+          Hopefully this experience
           teaches not only informtion about credit, but the power of decision making. 
           Happy Learning!
         </p>
